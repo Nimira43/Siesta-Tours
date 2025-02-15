723b142c6e9ca6cbc6a2be2cced5b67d2c3200b3
Nimira43 <n.rabone72@aol.com>
2025-02-15 22:58:15 +0000

    Links Dropdown menu done for the moment - mapping through the links array and displaying the nav-links.

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,8 +1,43 @@
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { Button } from '@/components/ui/button'
+import UserIcon from './UserIcon'
+import { RxHamburgerMenu } from 'react-icons/rx'
+import { links } from '@/utils/links'
+import Link from 'next/link'
+
 function LinksDropdown() {
   return (
-    <div>
-      <h1>Links Dropdown</h1>
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant='outline'
+          className='flex gap-4 max-w-[100px]'
+        >
+          <RxHamburgerMenu className='w-6 h-6' />         
+          <UserIcon />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className='w-52'
+        align='start'
+        sideOffset={10}
+      >
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem
+              key={link.href}
+            >
+              <Link
+                href={link.href}
+                className='capitalize w-full'
+              >
+                {link.label}
+              </Link>
+            </DropdownMenuItem>
+          )
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -2,7 +2,7 @@ import { HiOutlineUser } from 'react-icons/hi'
 
 function UserIcon() {
   return (
-    <HiOutlineUser className='w-6 h-6 bg-primary rounded-full text-light' />
+    <HiOutlineUser className='w-6 h-6  rounded-full text-light' />
   )
 }
 

eb15ac99f1e5440c8cb4f9a61143346e0f84f119
Nimira43 <n.rabone72@aol.com>
2025-02-15 23:49:19 +0000

    Starting work on SignOutLink.tsx - started off by bringing in useToast

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,4 +1,13 @@
+'use client'
+
+import { useToast } from '@/hooks/use-toast'
+
 function SignOutLink() {
+  const { toast } = useToast()
+  const handleLogout = () => {
+    toast({ description: 'You have been signed out.'})
+  }
+  
   return (
     <div>
       <h1>Sign Out Link</h1>

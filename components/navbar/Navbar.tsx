9469953b315b379b2cf2d180b11bf31ab34ee696
Nimira43 <n.rabone72@aol.com>
2025-02-15 21:31:36 +0000

    set up basic navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,7 +1,20 @@
+import DarkMode from "./DarkMode"
+import LinksDropdown from "./LinksDropdown"
+import Logo from "./Logo"
+import NavSearch from "./NavSearch"
+
 function Navbar() {
   return (
     <nav border-b>
-      <div className="container flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4 py-8"></div>
+      <div className="container flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4 py-8">
+        <Logo />
+        <NavSearch />
+        <div className="flex gap-4 items-center">
+          <DarkMode />
+          <LinksDropdown />
+        </div>
+      </div>
+      
       
     </nav>
   )

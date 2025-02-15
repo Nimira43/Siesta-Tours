80badda5957255814b7f2d57071c449810063d38
Nimira43 <n.rabone72@aol.com>
2025-02-15 21:02:32 +0000

    Cleaned up boilerplate

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        '--main': '#ff4500',
+        '--sky': '#87ceeb',
+        '--dark': '#111',
+        '--light': '#fffcfa',
+        '--grey-dark': '#333',
+        '--grey-medium': '#999',
+        '--grey-light': '#ccc',
+        '--grey-light-extra': '#eee'
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config

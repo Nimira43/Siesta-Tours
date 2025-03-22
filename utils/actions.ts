767a1a305261cfab6ed89a7b196d9160993c87a4
Nimira43 <n.rabone72@aol.com>
2025-03-22 15:43:34 +0000

    Removed old logic from createProfileAction and created a try catch block. Created rawData variable.

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,8 +1,12 @@
 'use server'
 
-export const createProfileAction = async (prevState: any, formData: FormData) => {
-  'use server'
-  const firstName = formData.get('firstName') as string
-  if (firstName !== 'John') return { message: 'An error has occurred.' }
-  return { message: 'Profile Created' }
+export const createProfileAction = async (
+  prevState: any,
+  formData: FormData
+) => {
+  try {
+    const rawData = Object.fromEntries(formData)
+  } catch (error) {
+
+  }
 }

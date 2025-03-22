2553c1f7809e6797e06604a4c27469fe250d5862
Nimira43 <n.rabone72@aol.com>
2025-03-22 15:45:09 +0000

    Imported profileSchema and set it to validatedFields.

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,11 +1,14 @@
 'use server'
 
+import { profileSchema } from './schemas'
+
 export const createProfileAction = async (
   prevState: any,
   formData: FormData
 ) => {
   try {
     const rawData = Object.fromEntries(formData)
+    const validatedFields = profileSchema
   } catch (error) {
 
   }

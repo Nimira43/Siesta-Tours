cf8624c6de87686d8ee0199bff4ae4bb8937c914
Nimira43 <n.rabone72@aol.com>
2025-03-22 18:39:51 +0000

    handling success or error in profile creation.

--- utils/actions.ts
+++ utils/actions.ts
@@ -9,7 +9,10 @@ export const createProfileAction = async (
   try {
     const rawData = Object.fromEntries(formData)
     const validatedFields = profileSchema.parse(rawData)
+    console.log(validatedFields)
+    return {message: 'Profile create.'}
   } catch (error) {
-
+    console.log(error)
+    return {message: 'There was an error.'}
   }
 }

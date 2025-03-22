529292853c1b21178202d1cad8cb590a1afeb46b
Nimira43 <n.rabone72@aol.com>
2025-03-22 15:36:06 +0000

    Defined profileSchema

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -2,5 +2,7 @@ import * as z from 'zod'
 import { ZodSchema } from 'zod'
 
 export const profileSchema = z.object({
-  firstName: z.string().max(5)
+  firstName: z.string().min(2),
+  lastName: z.string().min(2),
+  userName: z.string().min(2) 
 })
